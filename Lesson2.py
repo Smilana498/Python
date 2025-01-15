f174413c63ec7503827c062993e13c4d0696a76a
Smilana498 <mm6085058@gmail.com>
2025-01-15 18:04:25 +0600

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,21 @@
+# name = 'Tom'
+# name = 'Jack'
+# print(name)
+# print("Hello, 'Sam',!")
+# name = 'Sam'
+# Name = 'Tom'
+# print(name)
+
+# r = 10
+# Pi = 3.14
+# a = 2*Pi*r*r
+# print(a)
+
+# name = input("What is your name?")
+# surname = input("What is your surname?")
+# fullname = name+ " " + surname
+# print("Hello", fullname)
+
+# C = int(input("enter your celcium:"))
+# F = 9/5*C+32
+# print(F)
