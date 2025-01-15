500c7e0c6ecf578ccccaf7210edaca0722eccb9a
Smilana498 <mm6085058@gmail.com>
2025-01-15 18:02:24 +0600

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,5 @@
+# name = input("What is your name?")
+# age = input("How old are you?")
+# country = input("Where are you from?")
+# film = input ("What is your favorite film?")
+# print("His name is", name,",", "He is", age,",", "He is from", country,",", "His favorite film it is", film,"." )
