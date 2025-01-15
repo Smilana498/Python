395ce8ad9d6a371721d6d67095da54b76b9cbdba
Smilana498 <mm6085058@gmail.com>
2025-01-15 18:05:14 +0600

    Add files via upload

---
+++ Lesson4.py
@@ -0,0 +1,21 @@
+# num1 = str(5.6)
+# num2 = int(7.6)
+# num3 = num1 * num2
+# # num4 = num1 + num2
+# print(num3)
+
+
+# name = 'Mary'
+# age = 20
+# age = float(age)
+# age = str(age)
+# age = 20
+# print(type(age))
+
+Sales = str(input("Hello, its my flower shop, what do you need?"))
+Roses = float(100.05)
+Tulip = int(120)
+Sales2 = int(input("How much do you need?"))
+print(f"{Sales}\n  Roses costs: {Roses}\n"
+      f"Tulip costs: {Tulip}\n"
+      f"{Sales2}\n")
