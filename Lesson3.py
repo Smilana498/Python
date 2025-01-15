0d3ef7a44b80da6028f178ddd6cfd8ebea681455
Smilana498 <mm6085058@gmail.com>
2025-01-15 18:04:49 +0600

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,23 @@
+# Типы данных
+# int() -> 10, 3, 105..
+# float()-> 10.5, 2.123..
+# str() ->("..."), text
+# bool()m-> TRue or False
+
+# C = float(input("enter your celcium:"))
+# F = 9/5*C+32
+# A = int(F)
+# print("В цельсиях:", C,"C",  "В фар:", F, "F", "Целое:", int(F))
+# print(f"celcium: {C}, in foreghrits: {F}, f in integer: {int(F)}.")
+
+# num1 = 5
+# print(num1)
+# num2 = float(num1)
+# print(num2)
+
+# Apple
+# Salesman = int(input("Hello, we have gadgets, how much do you need?"))
+# Iphone = int(1000000)
+# Macbook = float(1000000.10)
+# print(f"{Salesman}\n I want buy Iphone, how much cost is it? It costs: {Iphone}\n"
+#       f" How much cost Macbook? It costs: {Macbook},\n  Thanks, i will think!")
